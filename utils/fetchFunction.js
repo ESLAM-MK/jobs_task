bd2da49d58edc95a4f17473615cac6ecc28b6f4d
Eslam Medhat Kamel <medhateslam886@gmail.com>
2026-06-29 22:22:54 +0300

    anthor fix

--- utils/fetchFunction.js
+++ utils/fetchFunction.js
@@ -1,7 +1,7 @@
  // i made general function to make it usable for get all jobs and get sepecific job
- const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
-const  fetchFun = async(slug)=>{ // dynamic data fetching function 
+ const  fetchFun = async(slug)=>{ // dynamic data fetching function 
     try {
+        const baseUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
         const fetchLink = (!slug) ? `${baseUrl}/jobs` : `${baseUrl}/jobs?slug=${slug}`
         const res = await fetch(fetchLink) // filteration by slug
         if (!res.ok) return null // if response of fetching not ok it will return null
@@ -9,7 +9,7 @@ const  fetchFun = async(slug)=>{ // dynamic data fetching function
         if (data.length === 0) return null
         return slug ? data[0] : data // this will return specific job depends on slug
     } catch (error) {
-        throw new Error(error.message)
-    }
+        console.error("Data fetching failed:", error.message);
+        return null;    }
 }
 export default fetchFun
